330fb0fd160e1e209f99acc96ef24cb03fe6d353
unknown <sanuj12201@gmail.com>
2025-12-31 22:55:57 +0530

    feat: implement login with JWT authentication

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 const bcrypt = require("bcrypt");
 const prisma = require("../prismaClient");
+const jwt = require("jsonwebtoken");
+
 
 exports.register = async (req, res) => {
   try {
@@ -38,6 +40,67 @@ exports.register = async (req, res) => {
     // 5. send response (never send password)
     return res.status(201).json({
       message: "User registered successfully",
+      user: {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      message: "Internal server error",
+    });
+  }
+};
+
+
+
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // 1. validation
+    if (!email || !password) {
+      return res.status(400).json({
+        message: "Email and password are required",
+      });
+    }
+
+    // 2. find user
+    const user = await prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (!user) {
+      return res.status(401).json({
+        message: "Invalid email or password",
+      });
+    }
+
+    // 3. compare password
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      return res.status(401).json({
+        message: "Invalid email or password",
+      });
+    }
+
+    // 4. generate JWT
+    const token = jwt.sign(
+      {
+        userId: user.id,
+        email: user.email,
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: "7d" }
+    );
+
+    // 5. response
+    return res.status(200).json({
+      message: "Login successful",
+      token,
       user: {
         id: user.id,
         name: user.name,
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,12 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-const { register } = require("../controllers/auth.controller");
+const { register, login } = require("../controllers/auth.controller");
 
 router.post("/register", register);
-
-router.post("/login", (req, res) => {
-  res.json({ message: "Login route" });
-});
+router.post("/login", login);
 
 module.exports = router;
